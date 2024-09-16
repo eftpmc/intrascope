351462e1778fb534ceed781fdc3f50ee5c5ae581
SaturnEFTPMC <zackhhi@gmail.com>
2024-09-16 12:26:18 -0400

    admin check

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,22 +2,24 @@
 
 import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
-import { checkSession } from '@/auth';
+import { getUserProfile, getUser } from '@/auth';
 import { DocumentsLayout } from '@/layouts/Documents'
 
 export default function DashboardPage() {
   const router = useRouter()
 
   useEffect(() => {
-    const checkUserSession = async () => {
-      const sessionData = await checkSession();
-      const session = sessionData.session;
-      if (session) {
-        router.push("/dashboard")
+    const fetchProfile = async () => {
+      const user = await getUser();
+      if (user) {
+        const profile = await getUserProfile(user.id);  // Use user id from getUser
+        if (profile && profile.role != 'admin') {
+          router.push("/dashboard")
+        }
       }
     };
 
-    checkUserSession();
+    fetchProfile();
   }, []);
 
   return (
